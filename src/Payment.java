500005188e106ed1c66a7d4644c4a77a3af91823
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-05-28 09:24:27 +0300

    feat: implement method: addAmount, isSufficient, deduct, getBalance in Payment

--- src/Payment.java
+++ src/Payment.java
@@ -4,4 +4,21 @@ public class Payment {
     public Payment() {
         this.amount = 0;
     }
+    public void addAmount(float value) {
+        this.amount += value;
+    }
+
+    public boolean isSufficient(float price) {
+        return amount >= price;
+    }
+
+    public void deduct(float price) {
+        if (isSufficient(price)) {
+            amount -= price;
+        }
+    }
+
+    public float getBalance() {
+        return amount;
+    }
 }
