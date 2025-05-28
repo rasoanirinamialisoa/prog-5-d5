8a0c506ddadef0b0d7acfbb6272b2b6c97261bf8
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-05-28 09:14:17 +0300

    feat: implement class Payment

---
+++ src/Payment.java
@@ -0,0 +1,15 @@
+public class Payment {
+    public float amount;
+
+    public Payment(float amount) {
+        this.amount = amount;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+}
