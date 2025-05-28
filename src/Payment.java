235cdbc62213ee16c4b93a47001cf6adb015149a
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-05-28 09:19:15 +0300

    chore: initialise amount = 0

--- src/Payment.java
+++ src/Payment.java
@@ -1,15 +1,7 @@
 public class Payment {
     public float amount;
 
-    public Payment(float amount) {
-        this.amount = amount;
-    }
-
-    public float getAmount() {
-        return amount;
-    }
-
-    public void setAmount(float amount) {
-        this.amount = amount;
+    public Payment() {
+        this.amount = 0;
     }
 }
