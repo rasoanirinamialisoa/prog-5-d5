6a20030a848f4d0712c3d758914fc45a66f3b2d6
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-05-28 09:46:44 +0300

    feat: create ipunt line choice in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bienvenue dans la machine à café CLI !");
+        while (true) {
+            System.out.println("\n1. Payer");
+            System.out.println("2. Choisir une boisson");
+            System.out.println("3. Quitter");
+            System.out.print("Choix: ");
+
+        }
     }
 }
